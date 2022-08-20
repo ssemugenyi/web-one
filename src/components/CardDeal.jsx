54989b9c2d615bfc2842d637cc040b59713f4a63
ssemugenyiisaac2@gmail.com <ssemugenyiisaac2@gmail.com>
2022-08-20 13:50:23 +0300

    COMPLETE WEBSITE

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px]`}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut blanditiis
+        doloremque ea sint dignissimos voluptates numquam odio dicta officia
+        dolores.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%] " />
+    </div>
+  </section>
+);
+
+export default CardDeal;
